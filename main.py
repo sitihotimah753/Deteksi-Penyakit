9f46c7da552c885ebf096917c07709f43b2f3a32
sitihotimah753 <khotimahkhotimah971@gmail.com>
2025-07-26 01:10:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -41,5 +41,6 @@ def predict():
     return render_template('result.html', **result)
 
 # Menjalankan aplikasi
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
+
